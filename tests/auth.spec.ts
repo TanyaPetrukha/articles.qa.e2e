053c09eb30a90f75bdf8d3f24c29cf55ea658767
Tetiana Petrukha <tan9onyschenko@gmail.com>
2026-09-20 14:11:25 +0300

    refactor: use BASE_URL instead of UI_URL

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-const { UI_URL, API_URL } = process.env;
+const {API_URL} = process.env;
 const username = `ttt+${new Date().getTime()}`;
 const email = `ttt_email+${new Date().getTime()}@gmail.com`;
 const password = "Test123!@#";
@@ -27,7 +27,7 @@ test.describe(
     test("01 user register successfully with unique credentials", async ({
       page,
     }) => {
-      await page.goto(`${UI_URL!}/register`);
+      await page.goto("/register");
       await page.getByTestId("auth-username").fill(username);
       await page.getByTestId("auth-email").fill(email);
       await page.getByTestId("auth-password").fill(password);
@@ -42,14 +42,14 @@ test.describe(
     test("02 registration form should show an error for an already registered email", async ({
       page,
     }) => {
-      await page.goto(`${UI_URL!}/register`);
+      await page.goto("/register");
       await page.getByTestId("auth-username").fill(username);
       await page.getByTestId("auth-email").fill(emailExist);
       await page.getByTestId("auth-password").fill(password);
       await page.getByTestId("register-confirm-password").fill(password);
       await page.getByTestId("register-terms").check();
       await page.getByTestId("auth-submit").click();
-      
+
       await expect(
         page
           .getByTestId("error-messages")
@@ -60,7 +60,7 @@ test.describe(
     test("03 registration form should show an error for empty email", async ({
       page,
     }) => {
-      await page.goto(`${UI_URL!}/register`);
+      await page.goto("/register");
       await page.getByTestId("auth-username").fill(username);
       await page.getByTestId("auth-email").fill("");
       await page.getByTestId("auth-password").fill(password);
@@ -79,7 +79,7 @@ test.describe("ART-002 Login", { tag: ["@auth", "@login"] }, () => {
   test("01 user should log in successfully with valid credentials", async ({
     page,
   }) => {
-    await page.goto(`${UI_URL!}/login`);
+    await page.goto("/login");
     await page.getByTestId("auth-email").fill(emailExist);
     await page.getByTestId("auth-password").fill(password);
     await page.getByTestId("auth-submit").click();
@@ -90,7 +90,7 @@ test.describe("ART-002 Login", { tag: ["@auth", "@login"] }, () => {
   test("02 login form should show an error for an incorrect password", async ({
     page,
   }) => {
-    await page.goto(`${UI_URL!}/login`);
+    await page.goto("/login");
     await page.getByTestId("auth-email").fill(emailExist);
     await page.getByTestId("auth-password").fill("Newpass123!!!");
     await page.getByTestId("auth-submit").click();
@@ -103,7 +103,7 @@ test.describe("ART-002 Login", { tag: ["@auth", "@login"] }, () => {
   });
 
   test("03 login should reject a non-existing user", async ({ page }) => {
-    await page.goto(`${UI_URL!}/login`);
+    await page.goto("/login");
     await page.getByTestId("auth-email").fill(emailNotExist);
     await page.getByTestId("auth-password").fill(password);
     await page.getByTestId("auth-submit").click();
